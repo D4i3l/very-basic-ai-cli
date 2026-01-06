33645c32361370a9b3e422cb9a36e7a9f1b478b4
D4i3l <danielerik640@gmail.com>
2026-01-06 10:31:31 +0100

    added api key input and updated readme

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # A very basic AI CLI app
 Written in python using the textual library.
 
-This is my first proper project, so don't expect much from that app...
+This is my first proper project, so don't expect much from this app...
 
 ## Future plans
 - [ ] Add chat history
-- [ ] Add api key input, so you don't have to insert it into the code
+- [x] Add api key input, so you don't have to insert it into the code
+- [ ] Add error handling and loading screens, so the app won't crash/freeze
 - [ ] Add some colors and life to it
+- [ ] add export chat to markdown
 
 # Installation
+**Run these commands:**
 ```bash
+# clone the repo
 git clone https://github.com/D4i3l/very-basic-ai-cli.git
+# enter the directory
 cd very-basic-ai-cli
+#install the dependencies 
 pip install -r requirements.txt
 ```
 
--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Input, MarkdownViewer, Header, Footer
+from textual.widgets import Input, MarkdownViewer, Header, Footer, Label
 from textual.binding import Binding
+from textual.screen import Screen
 from google import genai
 
-class mainapp(App):
-    BINDINGS = [Binding(key="ctrl+c", action="quit", description="exit")]
-    
+ApiKey = ""
+
+class ApiScreen(Screen):
+    BINDINGS = [Binding(key="ctrl+c", action="app.quit", description="exit", priority=True)]
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield Label("Please paste your Gemini API key.")
+        yield Input(placeholder="API key", id="apikey")
+
+    def on_mount(self) -> None:
+        self.title = "A very basic AI frontend"
+        self.sub_title = "For Google Gemini"
+
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        global ApiKey
+        if event.input.id == "apikey":
+            ApiKey = event.value
+            self.app.switch_screen(MainScreen())
+
+class MainScreen(Screen):
+    BINDINGS = [Binding(key="ctrl+c", action="app.quit", description="exit", priority=True)]
+    
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer(compact=True, show_command_palette=False)
         yield MarkdownViewer(id="disp-resp", show_table_of_contents=False)
         yield Input(placeholder="What is in your mind today?", id="usrinput")
+        self.notify(f"Api key changed to {ApiKey}.")
 
 
     def on_mount(self) -> None:
@@ -21,7 +43,7 @@ class mainapp(App):
         userinput = event.value
         
         def aiAgent() -> str:
-            API_KEY = "ADD YOUR API KEY HERE!!"
+            API_KEY = ApiKey
 
             client = genai.Client(vertexai = False, api_key=API_KEY)
 
@@ -37,6 +59,11 @@ class mainapp(App):
         viewer.document.update(md_content)
 
 
+class MainApp(App):
+    def on_mount(self) -> None:
+       self.push_screen(ApiScreen())
+       self.install_screen(MainScreen(), name="mainscreen")
+
 if __name__ =="__main__":
-    app = mainapp()
+    app = MainApp()
     app.run()
